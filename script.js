204fac2c970855b5ceb28adc4fd2ede0b0f48c8c
Kundan@6922 <u21cse1003@cit.ac.in>
2025-04-17 16:24:44 +0530

    update html, styles, responsive

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
+
+function openNav() {
+    document.getElementById("myTopnav").style.width = "60%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myTopnav").style.width = "0%";
+    
+  }
+  
